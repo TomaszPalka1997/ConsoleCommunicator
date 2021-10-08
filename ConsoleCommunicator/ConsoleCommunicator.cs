ccdd26277c1892c8b65bae1fe08e1d447d19420c
TomaszPalka1997 <tomasz.palka1997@gmail.com>
2021-10-08 21:33:28 +0200

    Serwer and View

--- ConsoleCommunicator/ConsoleCommunicator.cs
+++ ConsoleCommunicator/ConsoleCommunicator.cs
@@ -4,7 +4,42 @@ using System.Text;
 
 namespace ConsoleCommunicator
 {
+
     class ConsoleCommunicator
     {
+        public ConsoleCommunicator()
+        {
+            start();
+        }
+
+        void start()
+        {
+            ConsoleInterface();
+        }
+        void ConsoleInterface()
+        {
+            Console.WriteLine("Welcome in ConsoleCommunicator, make a choice");
+            Console.WriteLine("[1] to send a message");
+            Console.WriteLine("[2] to ad new contact");
+            Console.WriteLine("[3] to delete a contact");
+
+            int choice_ = Int32.Parse(Console.ReadLine());
+
+            ConsoleInterfaceSwitch(choice_);
+        }
+
+        void ConsoleInterfaceSwitch(int inputChoice)
+        {
+            switch (inputChoice)
+            {
+                case 1:
+                    Console.WriteLine("u have typed [1]");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
     }
 }
--- ConsoleCommunicator/Program.cs
+++ ConsoleCommunicator/Program.cs
@@ -4,31 +4,10 @@ namespace ConsoleCommunicator
 {
     class Program
     {
-        void ConsoleInterface ()
-        {
-            Console.WriteLine("Hello in ConsoleCommunicator, make a choice");
-            Console.WriteLine("[1] to send a message");
-            Console.WriteLine("[2] to ad new contact");
-            Console.WriteLine("[3] to delete a contact");
-           
-        }
-
-        void ConsoleInterfaceSwitch(int inputChoice)
-        {
-            switch (inputChoice)
-            {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-            }
-        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleCommunicator myConsoleCommunicator = new ConsoleCommunicator();
         }
     }
 }
